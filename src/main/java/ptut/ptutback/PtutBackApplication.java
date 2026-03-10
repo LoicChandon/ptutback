a71b72fdaa88c60d9fce37a02c2e7a02c7fa0b3f
Motis <vpnloicvpn@gmail.com>
2026-03-10 14:33:28 +0100

    first commit

---
+++ src/main/java/ptut/ptutback/PtutBackApplication.java
@@ -0,0 +1,13 @@
+package ptut.ptutback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtutBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtutBackApplication.class, args);
+	}
+
+}
